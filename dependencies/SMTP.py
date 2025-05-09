2bc0ae9d479ced9f91a75542e48b747a8049b0b5
Gapoly <norman@ikmail.com>
2025-05-09 11:31:21 +0000

    Update

---
+++ dependencies/SMTP.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+SMTP_SERVER = "smtp.server.com"
+SMTP_PORT = 465  # 465 for SSL/TLS - 587 for STARTTLS
+SENDER_EMAIL = "your_email@domain.com"
+SENDER_PASSWORD = "your_password"
+RECEIVER_EMAILS = [
+    "recipient1@domain.com",
+    "recipient2@domain.com"
+    # Ajoutez d'autres adresses ici
+]
---
+++ dependencies/docker_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# List of Docker images to monitor (format: "namespace/image")
+DOCKER_IMAGES = [
+    "library/image1",
+    "library/image2"
+]
